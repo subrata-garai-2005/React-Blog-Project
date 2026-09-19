2bdd071dff14f1f143239a035436ff4f1a0d5c1d
subrata-garai-2005 <subratagarai514@gmail.com>
2026-09-19 12:30:09 +0530

    Configure GitHub Pages deployment and GitHub Actions workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/React-Blog-Project/',
   plugins: [
     react(),
     tailwindcss(),
